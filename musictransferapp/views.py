46a74d986464d115ae8639da2d28213fad329c00
ReiterAdam <adamreiter@onmail.com>
2023-09-10 20:51:41 +0200

    Work on post request in form

--- musictransferapp/views.py
+++ musictransferapp/views.py
@@ -4,4 +4,25 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == "POST":
+        source = request.POST["source"]
+        destination = request.POST["destination"]
+        
+        match source:
+            case "spotify":
+                pass
+            case "apple-music":
+                pass
+            case "tidal":
+                pass
+            case "google-play":
+                pass
+            case "deezer":
+                pass
+            case _:
+                print("Unknown source!")
+
+                
+        return render(request, 'musictransferapp/index.html')
+
     return render(request, 'musictransferapp/index.html')
